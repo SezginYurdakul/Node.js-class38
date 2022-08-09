20fd00b4e506331554aa0745a4c6f181e0cab0c9
Sezgin Yurdakul <sezginyurdakul2022@gmail.com>
2022-08-09 10:03:55 +0200

    week1 practice ex is done

--- week1/practice-exercises/1-pad-numbers/padLeft.js
+++ week1/practice-exercises/1-pad-numbers/padLeft.js
@@ -4,8 +4,6 @@
  * e.g. padLeft('foo', 5, '_') -> '__foo'
  * e.g. padLeft(  '2', 2, '0')   -> '02'
  */
-function padLeft(val, num, str) {
+export function padLeft(val, num, str) {
 	return '00000'.replace(/0/g, str).slice(0, num - val.length) + val;
 }
-
-// YOUR CODE GOES HERE
